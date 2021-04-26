40a638b6b07690c314d419d0408b3a5e415c09a3
zoyinc <65219297+zoyinc@users.noreply.github.com>
2021-04-26 12:04:35 +1200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 getenv("HOSTNAME");
 $my_hostname = $_ENV["HOSTNAME"] ;
-print $my_hostname . "\n\"Hello, World!\"\nHere we go again....\nphp version is " . PHP_VERSION . "\n";
+print $my_hostname . "<br/>\"Hello, World!\"<br/>Here we go again....<br/>php version is " . PHP_VERSION . "\n";
 ?>
